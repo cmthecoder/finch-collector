c52e4c11ffccb3bb0e9c96f4d60c3dd050cbf0be
Chris Mems <christopher.mems@.com>
2022-11-04 06:48:56 -0500

    add css

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Finch:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('Lolo', 'tabby', 'Kinda rude.', 3),
+  Finch('Sachi', 'tortoiseshell', 'Looks cute.', 0),
+  Finch('Fancy', 'bombay', 'Happy feathers.', 4),
+  Finch('Bonk', 'selkirk rex', 'Chirps loudly.', 6)
+]
+
 # Create your views here.
 
 def home(request):
@@ -8,3 +22,6 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {'finches': finches})
